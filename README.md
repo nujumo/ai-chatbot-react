6294501eea9010d5c607ddbc6c044240da295e11
Nuala Monaghan <nuala_monaghan@hotmail.com>
2023-10-11 12:07:17 +0100

    initial commit of basic openai chatbot

---
+++ README.md
@@ -0,0 +1,28 @@
+# React + Vite
+
+This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
+
+Currently, two official plugins are available:
+
+- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
+- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+
+## Getting Started
+
+```sh
+# if using yarn v7 add --legacy-peer-deps option
+# see details here: https://github.com/storybookjs/storybook/issues/12983
+yarn install
+```
+
+Copy the .env file to a new .env.local file, and add your OpenAI API key.
+
+Now execute the following commands to run the app:
+
+```sh
+yarn dev
+```
+
+Now point your browser to http://localhost:5173/.
+
+
---
+++ src/App.jsx
@@ -0,0 +1,94 @@
+import * as React from 'react';
+import { OpenAI } from 'openai';
+
+import './index.css';
+
+// OpenApi
+const openai = new OpenAI({
+  apiKey: import.meta.env.VITE_OPENAI_API_KEY,
+  dangerouslyAllowBrowser: true,
+});
+
+function App() {
+  const [message, setMessage] = React.useState('');
+  const [chats, setChats] = React.useState([]);
+  const [isTyping, setIsTyping] = React.useState(false);
+
+  const chatsRef = React.useRef(null);
+
+  const scrollChat = () => {
+    console.log('xxx scrollToBottom');
+    chatsRef.current?.scrollTo(0, 1e10);
+  };
+
+  const chat = async (e, message) => {
+    e.preventDefault();
+
+    setIsTyping(true);
+    let messages = chats;
+    messages.push({ role: 'user', content: message });
+    // update chats with user input
+    setChats(messages);
+
+    scrollChat();
+    setMessage('');
+
+    await openai.chat.completions
+      .create({
+        model: 'gpt-3.5-turbo',
+        messages: [{ role: 'system', content: 'You are SupermarketCustomerGPT. You help with insights into customers shopping habits and campaign interactions.' }, ...chats],
+      })
+      .then((result) => {
+        messages.push(result.choices[0].message);
+        // update chats with bot response
+        setChats(messages);
+        scrollChat();
+        setIsTyping(false);
+      })
+      .catch((error) => console.log(error));
+  };
+
+  return (
+    <main className="flex flex-1 flex-col justify-items-stretch h-full w-full">
+      {/* <h1 className='text-3xl font-bold underline text-center'>React ChatGPT App</h1> */}
+      <h1 className="mb-4">React ChatGPT App</h1>
+      <section
+        className="flex flex-col flex-1 border box-border rounded-xl w-full px-4 pt-4 overflow-y-auto"
+        ref={chatsRef}
+      >
+        {chats && chats.length
+          ? chats.map((chat, index) => (
+            <p
+              key={index}
+              className={`flex flex-shrink mb-4 rounded-xl p-3 ${chat.role === 'user' ? 'flex-row-reverse text-right bg-gray-300' : 'flex-row bg-gray-600 text-white'
+                }`}
+            >
+              <span>{chat.role}</span>
+              <span>: </span>
+              <span>{chat.content}</span>
+            </p>
+          ))
+          : null}
+        {isTyping ? (
+          <div>
+            <p>
+              <i>Typing...</i>
+            </p>
+          </div>
+        ) : null}
+      </section>
+
+      <form onSubmit={(e) => chat(e, message)}>
+        <input
+          className="w-full p-2 mt-2 bg-slate-100 border border-cyan-950 rounded-xl"
+          name="message"
+          value={message}
+          placeholder="Type a message and hit enter"
+          onChange={(e) => setMessage(e.target.value)}
+        />
+      </form>
+    </main>
+  );
+}
+
+export default App;
